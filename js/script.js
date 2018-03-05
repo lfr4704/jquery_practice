ed5324466cee0b4b0852212cc32b66bacc2130ed
Luis <lfr4704@gmail.com>
2018-03-05 13:03:57 -0800

    added files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
-console.log("hello");
+$( document ).ready(function() { //this is how you call jquery
+	//the following is code to change color by id
+	//$("#header").click( function() {
+	//	$("h1").css("color", "red");
+	//})
+
+var currentColor = $("#header").css("color");
+
+if (currentColor == "rgb(0, 0, 0)") {
+
+	$("#header").css("color", "gold");
+} else {
+	$("#header").css('color', "rgb(0, 0, 0");
+}
+
+
+
+}); //this is how you end jquery
+
+	
+
 
 
 
