cc2a654f99219315481880774e892f0b4dfb495b
Luis <lfr4704@gmail.com>
2018-03-06 10:43:06 -0800

    revised

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ $( document ).ready(function() { //this is how you call jquery
 	//the following is code to change color by id
 	//$("#header").click( function() {
 	//	$("h1").css("color", "red");
-	//})
+	//});
+
+/*	this is how to change color of id header on click of header
+$("#header").click(function() {
 
 var currentColor = $("#header").css("color");
 
@@ -10,10 +13,25 @@ if (currentColor == "rgb(0, 0, 0)") {
 
 	$("#header").css("color", "gold");
 } else {
-	$("#header").css('color', "rgb(0, 0, 0");
+	$("#header").css('color', "rgb(0, 0, 0)");
 }
 
+});
+*/
 
+	var box = $("#my-box");
+
+	$("#show").click(function(){
+
+		box.show(1000);
+	});
+		
+
+	$("#hide").click(function(){
+
+		box.hide(1000);
+	});
+		
 
 }); //this is how you end jquery
 
